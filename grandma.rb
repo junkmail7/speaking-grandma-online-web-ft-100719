51afda300b6cbd5bc1d82e96ef37bfe5662d5935
William Pannell <lpann5@gmail.com>
2019-10-13 03:27:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -5,7 +5,8 @@ if (phrase.upcase==true)
   puts("HUH?! SPEAK UP, SONNY!")
 else if(phrase.downcase==true)
   puts("NO, NOT SINCE 1938!")
-else if
+else if(phrase=="I LOVE YOU GRANDMA!")
+  puts("")
 e
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
