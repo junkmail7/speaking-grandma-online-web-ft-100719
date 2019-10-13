edfb3a3bde1bc3e50c20f354aca5c88291e0251d
William Pannell <lpann5@gmail.com>
2019-10-13 03:28:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,8 +6,10 @@ if (phrase.upcase==true)
 else if(phrase.downcase==true)
   puts("NO, NOT SINCE 1938!")
 else if(phrase=="I LOVE YOU GRANDMA!")
-  puts("")
-e
+  puts("I LOVE YOU TOO PUMPKIN!")
+end
+
+end
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
