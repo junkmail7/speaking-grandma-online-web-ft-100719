fdcaf7f83451f5aa825dfcab2d17f57b6efeba75
William Pannell <lpann5@gmail.com>
2019-10-13 03:32:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,2 @@
 
-def speak_to_grandma(phrase="")
-  
-if (phrase.upcase==phrase)
-  puts("HUH?! SPEAK UP, SONNY!")
-else if(phrase.downcase==phrase)
-  puts("NO, NOT SINCE 1938!")
-else if(phrase=="I LOVE YOU GRANDMA!")
-  puts("I LOVE YOU TOO PUMPKIN!")
-end
-
-end
 
